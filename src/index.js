f4a9407f2f431ff60a99cd93897c3b9bf39c68c8
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-05-15 21:39:37 +0630

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./lib/db.js";
+import "dotenv/config";
+
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+
+app.listen(PORT, () => {
+  console.log("Listening on port 3000");
+  connectDB();
+});
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,103 @@
+import express from "express";
+import cloudinary from "../lib/cloudinary.js";
+import Book from "../models/Book.js";
+import protectRoute from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.post("/", protectRoute, async (req, res) => {
+  try {
+    const { title, caption, rating, image } = req.body;
+    if (!image || !title || !caption || !rating)
+      return res.status(400).json({ message: "Please provide all fields" });
+
+    //upload the image to cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
+
+    //save to the database
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      user: req.user._id,
+    });
+
+    await new Book.save();
+
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.log("Error creating book", error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//infinite loading
+router.get("/", protectRoute, async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find()
+      .sort({ createdAt: -1 }) //desc
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage");
+
+    const totalBooks = await Book.countDocuments();
+    res.send({
+      books,
+      currentPage: page,
+      totalBooks,
+      totalPages: Math.ceil(totalBooks / limit),
+    });
+  } catch (error) {
+    console.log("Error in getting books", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ message: "Book not found" });
+
+    //check if user is the creator of the book
+    if (book.user.toString() !== req.user._id)
+      return res.status(401).json({ message: "Unauthorized" });
+
+    // delete image from cloudinary
+    if (book.image && book.image.includes("cloudinary")) {
+      try {
+        const publicId = book.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (deleteError) {
+        console.log("Error in deleting image from cloudinary", deleteError);
+      }
+    }
+
+    await book.deleteOne();
+    res.json({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleting book", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// get recommended books by the logged in user
+router.get("/user", protectRoute, async (req, res) => {
+  try {
+    const books = await Book.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    });
+    res.json(books);
+  } catch (error) {
+    console.log("Get user books error", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+export default router;
